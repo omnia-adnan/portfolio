0fc286d6240e25df39f8c368f9876bcf640f56c6
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-04-02 11:09:55 +0300

    fixe navbar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let linkList = document.getElementById("link-list");
 let closeicon = document.getElementById("close-icon");
 let showicon = document.getElementById("show-icon");
+let logo = document.getElementById("cont-logo");
 
 function showList() {
     if (window.matchMedia("(min-width: 320px)").matches) {
@@ -8,10 +9,13 @@ function showList() {
         linkList.style.display = "none";
         closeicon.style.display = "none";
         showicon.style.display = "block";
+        logo.style.display = "block";
         } else {
         linkList.style.display = "block";
         closeicon.style.display = "block";
         showicon.style.display = "none";
+        logo.style.display = "none";
         }
     }
 }
+
